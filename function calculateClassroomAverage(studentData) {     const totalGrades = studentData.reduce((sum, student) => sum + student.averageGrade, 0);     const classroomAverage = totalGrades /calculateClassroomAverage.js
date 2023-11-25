422c97d6f4dd2219b8d15111a8306776f8a58cf0
LB45000 <144392042+LB45000@users.noreply.github.com>
2023-11-24 20:12:32 -0800

    Delete function calculateClassroomAverage(studentData) {     const totalGrades = studentData.reduce((sum, student) => sum + student.averageGrade, 0);     const classroomAverage = totalGrades  directory

--- function calculateClassroomAverage(studentData) {     const totalGrades = studentData.reduce((sum, student) => sum + student.averageGrade, 0);     const classroomAverage = totalGrades /calculateClassroomAverage.js
+++
@@ -1,7 +0,0 @@
-function calculateClassroomAverage(studentData) {
-    const totalGrades = studentData.reduce((sum, student) => sum + student.averageGrade, 0);
-    const classroomAverage = totalGrades / studentData.length;
-    return classroomAverage.toFixed(2);
-}
-
-export { calculateClassroomAverage };
